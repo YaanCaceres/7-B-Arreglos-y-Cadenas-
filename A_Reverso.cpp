53850d9b05727d30d785a2ad3b4431ba3d9c8219
YaanCaceres <ycaceresl@unjbg.edu.pe>
2026-06-22 20:00:40 -0500

    Agregue el ejericio A-Reverso

---
+++ A_Reverso.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int N;
+    cin >> N;
+    int arreglo[10];
+    for (int i = 0; i < N; i++) {
+        cin >> arreglo[i];
+    }
+    for (int i = N - 1; i >= 0; i--) {
+        cout << arreglo[i];
+        if (i > 0) {
+            cout << " ";
+        }
+    }
+    cout << endl; 
+    return 0;
+}
