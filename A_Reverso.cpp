d3f6787bd6789ade1696f5c929ce353cf62a0322
YaanCaceres <ycaceresl@unjbg.edu.pe>
2026-06-22 20:12:24 -0500

    Agregue texto para el usuario

--- A_Reverso.cpp
+++ A_Reverso.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int N;
-    cin >> N;
-    int arreglo[10];
-    for (int i = 0; i < N; i++) {
+    int n;
+    cout << "--- PROGRAMA DE SECUENCIA INVERSA ---" << endl;
+    cout << "Ingrese la cantidad de numeros que desea registrar: ";
+    cin >> n;
+    int arreglo[50]; 
+
+    cout << "\nPerfecto. Ahora ingrese los " << n << " numeros (puede presionar 'Enter' despues de cada uno):" << endl;
+    for (int i = 0; i < n; i++) {
+        cout << "numero " << i + 1 << ": ";
         cin >> arreglo[i];
     }
-    for (int i = N - 1; i >= 0; i--) {
+    cout << "\n=====================================" << endl;
+    cout << "La secuencia en orden inverso es:" << endl;
+
+    for (int i = n - 1; i >= 0; i--) {
         cout << arreglo[i];
+        
         if (i > 0) {
             cout << " ";
         }
     }
-    cout << endl; 
+    cout << "\n=====================================" << endl;
+    cout << "Programa finalizado." << endl;
+    
     return 0;
 }
