9d0cd573ef6c6e9f28007e97d972bd4ff3f7b9c4
YaanCaceres <ycaceresl@unjbg.edu.pe>
2026-06-22 20:47:54 -0500

    Agregue el ejercicio B_Polindromos

---
+++ B_Polindromos.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        char palabra[100]; 
+        cin >> palabra;
+        
+        int longitud = 0;
+        while (palabra[longitud] != '\0') { 
+            longitud++;
+        }
+        bool esPalindromo = true;
+        for (int j = 0; j < longitud / 2; j++) {
+            if (palabra[j] != palabra[longitud - 1 - j]) {
+                esPalindromo = false; 
+                break; 
+            }
+        }
+        if (esPalindromo) {
+            cout << "P" << endl;
+        } else {
+            cout << "NP" << endl;
+        }
+    }
+    return 0;
+}
